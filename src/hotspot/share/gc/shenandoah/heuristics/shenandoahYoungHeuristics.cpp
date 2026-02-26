cd8bf05e179d65b39765eceed633e14438e84316
Xiaolong Peng <xlpeng@amazon.com>
2026-02-25 20:29:31 -0800

    bytes_of_allocation_runway_before_gc_trigger should consider the remaining bytes of mutator allocator

--- src/hotspot/share/gc/shenandoah/heuristics/shenandoahYoungHeuristics.cpp
+++ src/hotspot/share/gc/shenandoah/heuristics/shenandoahYoungHeuristics.cpp
@@ -177,12 +177,14 @@ bool ShenandoahYoungHeuristics::should_start_gc() {
 size_t ShenandoahYoungHeuristics::bytes_of_allocation_runway_before_gc_trigger(size_t young_regions_to_be_reclaimed) {
   shenandoah_assert_heaplocked();
 
+  ShenandoahHeap* heap = ShenandoahHeap::heap();
   size_t capacity = _space_info->max_capacity();
-  size_t usage = _space_info->used();
-  size_t available = (capacity > usage)? capacity - usage: 0;
+  size_t mutator_allocator_remaining = heap->free_set()->mutator_allocator()->remaining_bytes();
+  size_t usage = _space_info->used() - mutator_allocator_remaining;
+  size_t available = (capacity > usage) ? capacity - usage: 0;
   size_t allocated = _space_info->bytes_allocated_since_gc_start();
 
-  size_t available_young_collected = ShenandoahHeap::heap()->collection_set()->get_young_available_bytes_collected();
+  size_t available_young_collected = heap->collection_set()->get_young_available_bytes_collected();
   size_t anticipated_available =
           available + young_regions_to_be_reclaimed * ShenandoahHeapRegion::region_size_bytes() - available_young_collected;
   size_t spike_headroom = capacity * ShenandoahAllocSpikeFactor / 100;
