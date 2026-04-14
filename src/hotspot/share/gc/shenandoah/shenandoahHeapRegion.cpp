3443ade7ec9482b5fbac1da3a8fe0fb71eb9fea5
Xiaolong Peng <xlpeng@amazon.com>
2026-04-13 17:49:20 -0700

    Revert unwanted changes

--- src/hotspot/share/gc/shenandoah/shenandoahHeapRegion.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahHeapRegion.cpp
@@ -584,6 +584,7 @@ void ShenandoahHeapRegion::recycle_internal() {
 // We may fail if some other thread recycled it before we do.
 void ShenandoahHeapRegion::try_recycle_under_lock() {
   shenandoah_assert_heaplocked();
+  if (!is_trash()) return;
   if (_recycling.try_set()) {
     if (is_trash()) {
       // At freeset rebuild time, which precedes recycling of collection set, we treat all cset regions as
@@ -604,6 +605,7 @@ void ShenandoahHeapRegion::try_recycle_under_lock() {
         os::naked_yield();
       }
     }
+    assert(!is_trash(), "Must not");
   }
 }
 
@@ -611,7 +613,9 @@ void ShenandoahHeapRegion::try_recycle_under_lock() {
 // some GC worker thread has taken responsibility to recycle the region, eventually.
 void ShenandoahHeapRegion::try_recycle() {
   shenandoah_assert_not_heaplocked();
+  if (!is_trash()) return;
   if (_recycling.try_set()) {
+    // Double check region state after win the race to set recycling flag
     if (is_trash()) {
       // At freeset rebuild time, which precedes recycling of collection set, we treat all cset regions as
       // part of capacity, as empty, as fully available, and as unaffiliated.  This provides short-lived optimism
