8952a56e7d820fb9ba459337eb6580ce47f6ece9
Xiaolong Peng <xlpeng@amazon.com>
2026-07-28 14:00:56 -0700

    Revert "unsafe_max_tlab_alloc always return ShenandoahHeapRegion::max_tlab_size_bytes()"
    This reverts commit 3b7682f1d79d1907730d8b2398ceeb792e65908f.

--- src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
@@ -1606,7 +1606,7 @@ void ShenandoahHeap::gclabs_retire(bool resize) {
 
 // Returns size in bytes
 size_t ShenandoahHeap::unsafe_max_tlab_alloc() const {
-  return ShenandoahHeapRegion::max_tlab_size_bytes();
+  return _allocator->unsafe_max_tlab_alloc(Thread::current());
 }
 
 size_t ShenandoahHeap::max_tlab_size() const {
